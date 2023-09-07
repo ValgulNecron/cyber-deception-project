62ba4aa7f3e930c7f902fec9cd83df9ecf18a97e
valgul <valgulnecron@gmail.com>
2023-09-07 18:57:15 +0200

    Started script.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # cyber-deception-project
+
+This repos contain all script, dockerfile and docker compose that are required or for QOL
---
+++ install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# this is the main script
+# it will execute other script and do some verification.
+
+
+echo "Do you want to proceed with the installation of Docker (yes/no)?:"
+read response
+case $response in
+  yes)
+    ./scripts/install_docker.sh
+    ./scripts/configure_docker.sh
+    echo "Switching to the created docker user..."
+    sudo su -l docker
+    echo "Do you want to build and launch all docker (yes/no)?:"
+    read response
+    case $response in
+    yes)
+
+      ;;
+    no)
+      # Cancel installation
+      echo "Installation cancelled."
+      exit 1
+      ;;
+    *)
+      echo "Invalid input. Please enter either 'yes' or 'no'."
+      exit 1
+      ;;
+  esac
+    ;;
+  no)
+    # Cancel installation
+    echo "Installation cancelled."
+    exit 1
+    ;;
+  *)
+    echo "Invalid input. Please enter either 'yes' or 'no'."
+    exit 1
+    ;;
+esac
+
