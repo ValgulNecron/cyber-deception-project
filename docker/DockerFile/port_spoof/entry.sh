663fa63aa924372b0a08c32cfcb178f8f2e29a22
valgul <valgulnecron@gmail.com>
2023-09-09 12:46:14 +0200

    WHY DOES THIS SHIT DOES NOT WORK

--- docker/DockerFile/port_spoof/entry.sh
+++ docker/DockerFile/port_spoof/entry.sh
@@ -4,6 +4,3 @@ set -e
 # Configure iptables
 iptables -t nat -A PREROUTING -p tcp --dport 1:79 -j DNAT --to-destination 127.0.0.1:4444
 
-ls /usr/local/etc/
-# Run your application
-/usr/local/bin/portspoof -s /usr/local/etc/portspoof_signatures
