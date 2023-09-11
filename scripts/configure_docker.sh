28063fcd706155a5dbcb9ec3047db29446ad23f6
valgul <valgulnecron@gmail.com>
2023-09-11 21:00:15 +0200

    Updated some thing

--- scripts/configure_docker.sh
+++ scripts/configure_docker.sh
@@ -3,12 +3,4 @@
 echo "Creating network..."
 sudo docker network create --subnet=172.20.0.0/16 main > /dev/null
 
-echo "Creating docker user..."
-sudo useradd docker > /dev/null
-sudo passwd docker
-
-echo "Adding docker user to the denied user of ssh..."
-echo "DenyUsers docker" >> /etc/ssh/sshd_config
-
-echo "Please restart the ssh service with 'sudo service ssh restart' or 'sudo systemctl restart ssh'"
 
--- scripts/launch_docker.sh
+++ scripts/launch_docker.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-
 echo "Launching management docker."
 # Launch management service
-docker-compose -f compose-management.yml up -d --build
+docker compose -f ../docker/compose-management.yml up -d --build
 
 echo "Launching honeypot docker."
 # Launch honeypot service
-docker-compose -f compose-honeypot.yml up -d --build
+docker compose -f ../docker/compose-honeypot.yml up -d --build
 
 # Launch active defence service
 echo "Launching other active defence docker."
-docker-compose -f compose-active-defence.yml up -d --build
+docker compose -f ../docker/compose-active-defence.yml up -d --build
 
 echo "Launching other docker."
 # Launch other service
-docker-compose -f compose-other.yml up -d --build
+docker compose -f ../docker/compose-other.yml up -d --build
 
