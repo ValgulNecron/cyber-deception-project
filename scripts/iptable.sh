f6383fa7a3bdc441ce899815dd758aef62874cf1
valgulnecron <valgulnecron@gmail.com>
2023-09-29 09:33:33 +0200

    new script

--- scripts/iptable.sh
+++ scripts/iptable.sh
@@ -15,6 +15,7 @@ for i in $(seq 1 65535); do
   sudo iptables -t nat -A PREROUTING -p tcp --dport $i -j DNAT --to-destination 127.0.0.1:4444
   sudo iptables -t nat -A OUTPUT -p tcp --dport $i -j DNAT --to-destination 127.0.0.1:4444
   sudo iptables-save
+  echo $i
   sleep 1s
 done
 # Save the rules
